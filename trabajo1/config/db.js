ef11390d555371b7903a1c3a6f5c0b15468be957
Monse-Alarcón <124050107@upq.edu.mx>
2026-02-06 22:02:22 -0600

    db.js (YA con pg)

--- trabajo1/config/db.js
+++ trabajo1/config/db.js
@@ -1,21 +1,18 @@
-const mysql = require('pg');
+const { Pool }  = require('pg');
 require('dotenv').config();
 
-const pool = mysql.createPool({
+const pool = new Pool({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
     port: process.env.DB_PORT,
-    waitForConnections: true,
-    connectionLimit: 10,
-    queueLimit: 0
 });
 
-pool.getConnection()
-    .then(connection => {
-        pool.releaseConnection(connection);
-        console.log('MYSQL conectado exitosamente');
+pool.connect()
+    .then(client => {
+        console.log('PostgreSQL conectado exitosamente');
+        client.release();
     })
     .catch(err => console.error('Error de conexión: ', err.code));
 
